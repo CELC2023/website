51b40a400a2cf00a07161739786ef7d0239adc2c
Robert Brown <brown.rjb75@gmail.com>
2021-12-27 22:10:17 -0700

    fix: update package file

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,10 +8,10 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 
-import Header from "./Header"
 import Footer from "./Footer"
+import Header from "./Header"
 
-const Layout = ({ children }) => {
+const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Header />
