604c74dac6fbfdf070963bed7e84279c772714a4
Robert Brown <brown.rjb75@gmail.com>
2022-03-03 08:57:58 -0700

    feat: update translations

--- src/components/TopArea.tsx
+++ src/components/TopArea.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import './TopArea.scss'
 
 const TopArea: React.FC = () => {
@@ -56,7 +56,7 @@ const TopArea: React.FC = () => {
             </div>
             <div className="information-card">
                 <div className="card-header">
-                    <h2 className='title-text'>{t('title-text')}</h2>
+                    <h2 className='title-text'><Trans i18nKey={'title-text'}></Trans></h2>
                     <h4 className='title-showcase-text'>{t('title-showcase')}</h4>
                 </div>
                 <p className='card-body'>{t('title-description')}</p>
