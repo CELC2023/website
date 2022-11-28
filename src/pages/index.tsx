94cfccb31637a673c370adee2324d5b24c77e006
Robert Brown <brown.rjb75@gmail.com>
2022-11-27 20:48:57 -0700

    feat: test tage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { graphql } from "gatsby"
-import * as React from "react"
+import { graphql, navigate, PageProps } from "gatsby"
+import React, { useEffect } from "react"
 import Attend from "../components/Attend"
 import BandsCombined from "../components/BandsCombined"
 import CommitteeListing from "../components/CommitteeListing"
@@ -14,31 +14,46 @@ import TopArea from "../components/TopArea"
 
 import "../styles/common.scss"
 
-const IndexPage: React.FC = () => (
-  <Layout>
-    <Seo title="Diversity, Technology, and Sustainability" />
-    <TopArea />
-    <BandsCombined />
-    <History />
-    <HowToAttend />
-    <Attend /> 
-    <Sponsors />
-    <FAQ />
-    <CommitteeListing />
-  </Layout>
-)
+const IndexPage: React.FC<PageProps> = (props: PageProps) => {
+  useEffect(() => {
+    const location = props.location.href
+    if(location != null) {
+      const url: URL = new URL(location);
+      const user = url.searchParams.get("user")
+      if(user != null && typeof window !== undefined) {
+        localStorage.setItem("user", user)
+        navigate("/tags")
+      }
+      
+    }
+  }, [])
+  
+  return (
+    <Layout>
+      <Seo title="Diversity, Technology, and Sustainability" />
+      <TopArea />
+      <BandsCombined />
+      <History />
+      <HowToAttend />
+      <Attend />
+      <Sponsors />
+      <FAQ />
+      <CommitteeListing />
+    </Layout>
+  )
+}
 
 export default IndexPage
 export const query = graphql`
-query($language: String!) {
-  locales: allLocale(filter: {language: {eq: $language}}) {
-    edges {
-      node {
-        ns
-        data
-        language
+  query($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
       }
     }
   }
-}
-`;
+`
